b970496e6767b87c2d580162aadaaabc8af0b5bf
vladian95 <kvankvan1995@gmail.com>
2022-09-21 19:07:42 +0200

    added adaptive header and first block

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
-"use strict"
+'use strict';
 
+const burgerMenu = document.querySelector('.header__burger');
+const mobileMenu = document.querySelector('.header__menu--mobile');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('header__burger--active');
+  mobileMenu.classList.toggle('header__burger--active');
+});
