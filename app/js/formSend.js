7093ede968f91a039c10627c89f249437806b6d7
vladian95 <kvankvan1995@gmail.com>
2022-10-24 13:29:10 +0200

    added

--- app/js/formSend.js
+++ app/js/formSend.js
@@ -1,33 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
+function SendMail() {
+  let = params = {
+    from_name: document.getElementById('inpName').value,
+    email_id: document.getElementById('emailId').value,
+    message: document.getElementById('message').value,
+  };
 
-  async function formSend(e) {
-    e.preventDefault();
-
-    // let error = formValidate(form);
-
-    let formData = new FormData(form);
-    // formData.append('image', formImage.files[0]);
-
-    if (error === 0) {
-      //   form.classList.add('_sending');
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData,
-      });
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        formPreview.innerHTML = '';
-        form.reset();
-        // form.classList.remove('_sending');
-      } else {
-        alert('Ошибка');
-        // form.classList.remove('_sending');
-      }
-    } else {
-      alert('Заполните обязательные поля');
-    }
-  }
-});
+  emailjs
+    .send('service_4pk5vtu', 'template_9tzjkpt', params)
+    .then(function (res) {
+      alert('success ' + res.status);
+    });
+}
